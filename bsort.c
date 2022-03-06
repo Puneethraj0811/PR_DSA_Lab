732f4f925e570f0326bf0f2a51658cc7e8880e26
Puneethraj K <79778679+Puneethraj0811@users.noreply.github.com>
2022-03-06 20:55:47 +0530

    Add files via upload

---
+++ bsort.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int a[10], n, i, j, temp;
+bsort()
+{
+	for(i=0;i<n-1;i++)
+	{
+		for(j=0;j<n-i-1;j++)
+		if(a[j]>a[j+1])
+		{
+			temp=a[j];
+			a[j]=a[j+1];
+			a[j+1]=temp;
+		}
+	}
+}
+int main()
+{
+	printf("Enter the number of elements:");
+	scanf("%d",&n);
+	printf("Enter the array elements:\n");
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	bsort();
+	printf("The sorted array is:\n");
+	for(i=0;i<n;i++)
+	printf("%d\t",a[i]);
+	printf("\n");
+	return 0;
+} 
+
+
---
+++ ssort.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int a[10], i, j, n, temp, min;
+ssort()
+{
+	for(i=0;i<n;i++)
+	{
+		min=i;
+		for(j=i+1;j<n;j++)
+		if(a[min]<a[j])
+		min=j;
+		temp=a[min];
+		a[min]=a[i];
+		a[i]=temp;
+
+	}
+}
+int main()
+{
+	printf("Enter the number of elements:");
+	scanf("%d",&n);
+	printf("Enter the array elements:\n");
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	ssort();
+	printf("The sorted array is:\n");
+	for(i=0;i<n;i++)
+	printf("%d\t",a[i]);
+	printf("\n");
+	return 0;
+}
