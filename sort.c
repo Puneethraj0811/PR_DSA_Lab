970fbefb3d4970c288ee3004dadb02bee7cac1e6
Puneethraj K <79778679+Puneethraj0811@users.noreply.github.com>
2022-05-30 20:24:05 +0530

    Add files via upload

---
+++ sort.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+int a[10], n, i, j, temp;
+void bsort()
+{
+	for(i=0;i<n-1;i++)
+	{
+		for(j=0;j<n-i-1;j++)
+		if(a[j]>a[j+1])
+		{
+			temp=a[j];
+			a[j]=a[j+1];
+			a[j+1]=temp;
+		}
+	}
+}
+
+void ssort()
+{
+	int min;
+	for(i=0;i<n;i++)
+	{
+		min=i;
+		for(j=i+1;j<n;j++)
+		if(a[min]<a[j])
+		min=j;
+		temp=a[min];
+		a[min]=a[i];
+		a[i]=temp;
+
+	}
+}
+
+void main()
+{
+	int op=1,ch;
+	printf("Enter the number of elements:");
+	scanf("%d",&n);
+	printf("Enter the array elements:\n");
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	while(op)
+	{
+		printf("1.Bubble Sort\n2.Selection Sort\n");
+		printf("Enter your choice:");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1: bsort();
+				printf("The sorted array is:\n");
+				for(i=0;i<n;i++)
+				printf("%d\t",a[i]);
+				printf("\n");
+				break;
+			case 2: ssort();
+				printf("The sorted array is:\n");
+				for(i=0;i<n;i++)
+				printf("%d\t",a[i]);
+				printf("\n");
+				break;
+			default:printf("Inavlid choice\n");
+				break;
+		}
+		printf("Do you want to continue(0/1):");
+		scanf("%d",&op);
+	}		
+}
