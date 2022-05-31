674af812c4f14f4a0183e9da573c233f27878ea4
Puneethraj K <79778679+Puneethraj0811@users.noreply.github.com>
2022-05-31 16:33:10 +0530

    Update SPushPop.c

--- SPushPop.c
+++ SPushPop.c
@@ -2,16 +2,13 @@
 #include<stdlib.h>
 #define max 3
 int top=-1,s[max],ch,ele,i;
+
 int push()
 {
 	printf("Enter the element to push\n");
 	scanf("%d",&ele);
-	if(top==max-1)
-	{
-		printf("Stack is full\n");
-		return 0;
-	}
 	s[++top]=ele;
+
 }
 int pop()
 {
@@ -36,16 +33,23 @@ void display()
 		printf("%d\t",s[i]);
 	}
 }
-void main()
+int main()
 {
 	int ch,opt=1;
 	while(opt)
 	{
-		printf("\n1.Push\t2.Pop\t3.Display\t4.Exit\nEnter your choice:");
+		printf("1.Push\n2.Pop\n3.Display\n4.Exit\nEnter your choice:");
 		scanf("%d",&ch);
 		switch(ch)
 		{
-			case 1: push();
+			case 1: if(top==max-1)
+				{	
+					printf("Stack is full\n");
+				}
+				else
+				{
+					push();
+				}
 			        break;
 			case 2: pop();
 			        break;
@@ -58,5 +62,4 @@ void main()
 		printf("\nDo you want to continue(0/1):");
 		scanf("%d",&opt);
 	}
-}			
-		
+}						
