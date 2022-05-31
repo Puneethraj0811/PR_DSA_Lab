249a6ddc6f15ee9de91845dedbd44fb655ec4c0e
Puneethraj K <79778679+Puneethraj0811@users.noreply.github.com>
2022-05-31 19:31:22 +0530

    Update infixtopostfix.c

--- infixtopostfix.c
+++ infixtopostfix.c
@@ -1,74 +1,78 @@
 #include<stdio.h>
 #include<ctype.h>
-char infix[30],postfix[30];
+char infix[30],postfix[30],stack[30];
 int top=-1;
-char stack[30];
 void push(char symbol)
 {
-        stack[++top]=symbol;
+	stack[++top]=symbol;
 }
 char pop()
 {
-        //char symbol;
-       // symbol=stack[top--];
-       // return symbol;
-       return stack[top--];
+	return stack[top--];
 }
 int preceed(char symbol)
 {
-        switch(symbol)
-        {
-                case '$':return -1;
-                case '(':return 0;
-                case '+':
-                case '-':return 1;
-                case '*':
-                case '/':return 2;
-                case '^':return 3;
-        } 
+	switch(symbol)
+	{
+		case '$': return -1;
+		case '(': return 0;
+		case '+':
+		case '-': return 1;
+		case '/': 
+		case '*': return 2;
+		case '^': return 3;
+	}
 }
-void infixtopostfix()
+void intopo()
 {
-        int i=0,k=0;
-        char symbol;
-        while((symbol=infix[i++])!='\0')
-        {
-                if(symbol=='(')
-                push(symbol);
-                else if(symbol==')')
-                {
-                        while(stack[top]!='(')
-                        {
-                                postfix[k++]=pop();
-                        }
-                        if(stack[top]=='(')
-			pop();
-                }
-                else if(isalnum(symbol))
-                {
-                        postfix[k++]=symbol;
-                }
-                else
-                {
-                        while(preceed(symbol)<=preceed(stack[top]))
-                        {
-                        postfix[k++]=pop();
-                        }
-                        push(symbol);
-                }
-        }
-        while(top !=0)
+	int i=0,k=0;
+	char symbol;
+	while((symbol=infix[i++])!='\0')
+	{
+		if(symbol=='(')
+		{
+			push(symbol);
+		}
+		else if(symbol==')')
+		{	
+			while(stack[top]!='(')
+			{
+				postfix[k++]=pop();
+			}
+			if(stack[top]=='(')
+			{
+				pop();
+			}
+		}
+		else if(isalnum(symbol))
+		{
+			postfix[k++]=symbol;
+		}
+		else
+		{
+			while(preceed(symbol)<=preceed(stack[top]))
+			{
+				postfix[k++]=pop();
+			}
+			push(symbol);
+		}
+	}
+	while(top!=-1)
 	{
 		if(stack[top]=='('||stack[top]==')')
-		top--;
+		{
+			top--;
+		}
 		else
-		postfix[k++]=pop();
-        }
+		{
+			postfix[k++]=pop();
+		}
+	}
 }
 void main()
 {
-        printf("Enter infix expression:");
-        scanf("%s",infix);
-        infixtopostfix();
-        printf("The postfix expression is:%s\n",postfix);
+	printf("Enter infix expression\n");
+	scanf("%s",infix);
+	intopo();
+	printf("Postfix expression is %s\n",postfix);
 }
