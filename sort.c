7a5c991a193b1002356f2bfbd4d6d9161ff71d2f
Puneethraj K <79778679+Puneethraj0811@users.noreply.github.com>
2022-06-06 07:43:02 +0530

    Update sort.c

--- sort.c
+++ sort.c
@@ -17,16 +17,22 @@ void bsort()
 void ssort()
 {
 	int min;
-	for(i=0;i<n;i++)
+	for(i=0;i<n-1;i++)
 	{
 		min=i;
 		for(j=i+1;j<n;j++)
-		if(a[min]<a[j])
-		min=j;
-		temp=a[min];
-		a[min]=a[i];
-		a[i]=temp;
-
+		{	
+			if(a[min]<a[j])
+			{
+				min=j;
+			}
+		}
+		if(min!=i)
+		{
+			temp=a[i];
+			a[i]=a[min];
+			a[min]=temp;
+		}
 	}
 }
 
